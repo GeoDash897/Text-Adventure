8e79def5a274202bff331b63c366baf15743ad84
GeoDash897 <mattjroman03@gmail.com>
2022-08-15 16:35:42 -0400

    Add ability to examine capsule glass
    In making this game, I am designing it around the philosophy that
    the player should actively engage with the world around them
    in order to progress- learning not only about the world's lore,
    but also what to do with it (objectives and things of that nature).
    
    In order to encourage this from the very start, I made the
    player's knowledge of the world a small as possible-
    being stuck inside of a tube. By creating the glass to examine,
    I hope to encourage the player to explore and interact with
    as many things as they can when playing the game.

--- game/data.js
+++ game/data.js
@@ -12,6 +12,7 @@ createItem("player", PLAYER(), {
 
 createRoom("teleStationInside", {
   headingAlias: "???",
+  discoveredButton: false,
   desc: function() {
     let start = "{once:You open up your eyes, dazed and confused about your current surroundings.|After rubbing the fuzziness out "
     + "of your vision, you realize that you are in :You are in }";
@@ -19,6 +20,32 @@ createRoom("teleStationInside", {
     + "outside of various colored blurs, but just enough so that some light can filter through the space.|"
     + "You can't make out much of the tube in this lighting environment, but you can see two orb-like structures "
     +"connected by wires toward the bottom left and right corners of your vision respectively.";
+    if(this.discoveredButton) {
+      middle += " Alongside this, there is a small control panel to the left of the barrier.";
+    }
     return start+middle; //Need to return string so that it can be formatted in room Template function
   },
 })
+
+createItem("stationGlass", {
+  loc: "teleStationInside",
+  alias: "glass barrier",
+  synonyms: ["glass"],
+  scenery: true,
+  state:0,
+  descs:[
+    "A some-what solid barrier of very old, dilapidated glass with two walls on either side.|"+
+    "At one time, it would have allowed a person to view the outside of the capsule. " +
+    "Due to the neglect of time however, there are various stains and cracks that now cover the glass, making it hard to see anything "+
+    "beyond...| However, through this more intensive search, you are able to see a small control "+
+    "panel to the left of the glass barrier that you did not make out earlier.",
+    "You try once more to see through the dirty glass pane with all of your might, but you only achieve a minor headache in the process.",
+  ],
+  examine: function() {
+    if(this.state === 0) {
+      this.scenery = false;
+      w.teleStationInside.discoveredButton = true;
+    }
+    msg("{cycleEnd:stationGlass:descs:state}");
+  }
+}) 
