de541c02781a1aca94178bdd55c83d4abec8c8cd
GeoDash897 <mattjroman03@gmail.com>
2022-08-15 16:22:58 -0400

    Update title and author of game

--- game/settings.js
+++ game/settings.js
@@ -1,7 +1,7 @@
 "use strict"
 
-settings.title = "Your new game"
-settings.author = "Your name"
+settings.title = "Las Pegasus Island"
+settings.author = "GeoDash"
 settings.version = "0.1"
 settings.thanks = []
 settings.warnings = "No warnings have been set for this game."
