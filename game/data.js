84212f03043099cca6b6c92db7871732e6aebe77
GeoDash897 <mattjroman03@gmail.com>
2022-08-15 16:24:46 -0400

    Add starting text descriptions of game
    As the player starts in an enclosed tube at the start of the game,
    I updated the initial room name and description in order to
    better match that scenario.
    
    Instead of making the title of the starting room
    "Inside Teleportation Station" or something like that, I decided
    to leave it as "???" in order to add mystery at the start,
    to make the player question what is going on.

--- game/data.js
+++ game/data.js
@@ -1,11 +1,24 @@
 "use strict"
 
-createItem("me", PLAYER(), {
-  loc:"lounge",
-  synonyms:['me', 'myself'],
-  examine: "Just a regular guy.",
+createItem("player", PLAYER(), {
+  loc:"teleStationInside",
+  synonyms:['me', 'myself', 'player'],
+  examine: function() {
+    let temp = "You look down at yourself, seeing two arms on the sides of your vision...|After realizing that you look a bit"
+    + "...{i:strange}...staring down at your arms, you stop in an attempt to look more normal.";
+    msg(temp);
+  }
 })
 
-createRoom("lounge", {
-  desc:"The lounge is boring, the author really needs to put stuff in it.",
+createRoom("teleStationInside", {
+  headingAlias: "???",
+  desc: function() {
+    let start = "{once:You open up your eyes, dazed and confused about your current surroundings.|After rubbing the fuzziness out "
+    + "of your vision, you realize that you are in :You are in }";
+    let middle = "some kind of enclosed gold metal tube. There is a glass barrier in front of you, too dirty to see the adjacent room beyond "
+    + "outside of various colored blurs, but just enough so that some light can filter through the space.|"
+    + "You can't make out much of the tube in this lighting environment, but you can see two orb-like structures "
+    +"connected by wires toward the bottom left and right corners of your vision respectively.";
+    return start+middle; //Need to return string so that it can be formatted in room Template function
+  },
 })
